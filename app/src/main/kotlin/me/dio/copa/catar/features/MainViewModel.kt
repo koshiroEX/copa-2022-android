844e67646e5912c2b7c0a7a123d2cbbd5b2f8915
Marcio "Koshiro" Menezes <118092843+koshiroEX@users.noreply.github.com>
2023-10-21 21:00:23 -0300

    add screen, workers

---
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
@@ -0,0 +1,80 @@
+package me.dio.copa.catar.features
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import me.dio.copa.catar.core.BaseViewModel
+import me.dio.copa.catar.domain.model.Match
+import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.usecase.DisableNotificationUseCase
+import me.dio.copa.catar.domain.usecase.EnableNotificationUseCase
+import me.dio.copa.catar.domain.usecase.GetMatchesUseCase
+import me.dio.copa.catar.remote.NotFoundException
+import me.dio.copa.catar.remote.UnexpectedException
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getMatchesUseCase: GetMatchesUseCase,
+    private val disableNotificationUseCase: DisableNotificationUseCase,
+    private val enableNotificationUseCase: EnableNotificationUseCase,
+): BaseViewModel<MainUIState, MainUiAction>(MainUIState()) {
+
+    init {
+        fetchMatches()
+    }
+
+
+    private fun fetchMatches() = viewModelScope.launch {
+        getMatchesUseCase()
+            .flowOn(Dispatchers.Main)
+            .catch {
+                when(it) {
+                    is NotFoundException -> sendAction(MainUiAction.MatchesNotFound(it.message ?: "Erro desconhecido"))
+                    is UnexpectedException -> sendAction(MainUiAction.Unexpected)
+                }
+            }
+            .collect {
+                setState {
+                    copy(matches = it)
+                }
+            }
+    }
+
+    fun toggleNotification(match: Match) {
+        viewModelScope.launch {
+            runCatching {
+                withContext(Dispatchers.Main) {
+                    val action = if (match.notificationEnabled) {
+                        disableNotificationUseCase(match.id)
+                        MainUiAction.DisableNotification(match)
+
+                    } else {
+                        enableNotificationUseCase(match.id)
+                        MainUiAction.EnableNotification(match)
+                    }
+
+                    sendAction(action)
+                }
+            }
+        }
+    }
+}
+
+
+
+data class MainUIState(
+    val matches: List<MatchDomain> = emptyList()
+)
+
+
+sealed class MainUiAction {
+    data class MatchesNotFound(val message: String) : MainUiAction()
+    object Unexpected: MainUiAction()
+    data class EnableNotification(val match: MatchDomain) : MainUiAction()
+    data class DisableNotification(val match: MatchDomain) : MainUiAction()
+}
